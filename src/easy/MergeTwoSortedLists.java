97d5a2746cf4991c829758cc932b9cfe58dc0136
Yiyun <yangyiyun@yougola.com>
2019-07-05 10:33:17 +0800

    21. update with cleaner solution

--- src/easy/MergeTwoSortedLists.java
+++ src/easy/MergeTwoSortedLists.java
@@ -17,6 +17,7 @@ import static java.lang.System.out;
  *
  * TODO: 1. consider equal case
  * TODO: 2. consider l1.length and l2.length: so there is the "if (l1.next == null) l1.next = l2"
+ * TODO: 3. mergeTwoLists_1 and mergeTwoLists_2 are cleaner solutions
  */
 public class MergeTwoSortedLists {
 
@@ -36,16 +37,6 @@ public class MergeTwoSortedLists {
     }
 
     private void merge(ListNode l1, ListNode l2) {
-//        out.print("l1: " );
-//        print(l1);
-//        out.print(", l2: ");
-//        print(l2);
-//        out.print(", originalL1: " );
-//        print(originalL1);
-//        out.print(", originalL2: ");
-//        print(originalL2);
-//        out.println();
-
         if (l1.next == null) {
             l1.next = l2;
             return;
@@ -54,8 +45,7 @@ public class MergeTwoSortedLists {
         if (l2 == null)
             return;
 
-        int first = l2.val;
-        if (l1.next.val <= first)
+        if (l1.next.val <= l2.val)
             merge(l1.next, l2);
         else {
             ListNode newL2 = l1.next;
@@ -86,9 +76,39 @@ public class MergeTwoSortedLists {
         }
     }
 
+    /** l: left; r: right; {l1},{l2}
+     * (l1->2->l4),(r1->3->r4)
+     * l1 -> (2->l4),(r1->3->r4)
+     *       r1 -> (2->l4),(3->r4)
+     *             2 -> (l4),(3->r4)
+     *                  3 -> (l4),(r4)
+     *                       l4 -> null,(r4)
+     *                             r4
+     */
+    public ListNode mergeTwoLists_1(ListNode l1, ListNode l2){
+        if(l1 == null) return l2;
+        if(l2 == null) return l1;
+        if(l1.val < l2.val){
+            l1.next = mergeTwoLists_1(l1.next, l2);
+            return l1;
+        } else{
+            l2.next = mergeTwoLists_1(l1, l2.next);
+            return l2;
+        }
+    }
+
+    public ListNode mergeTwoLists_2(ListNode l1, ListNode l2){
+        if (l1 == null) return l2;
+        if(l2 == null) return l1;
+        if (l1.val > l2.val)
+            return mergeTwoLists_2(l2, l1);
+        l1.next = mergeTwoLists_2(l1.next, l2);
+        return l1;
+    }
+
     @Test
     public void case1() {
-        ListNode listNode = mergeTwoLists(
+        ListNode listNode = mergeTwoLists_2(
                 new ListNode(1, new ListNode(2, new ListNode(4))),
                 new ListNode(1, new ListNode(3, new ListNode(4)))
         );
