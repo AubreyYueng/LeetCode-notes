18779e5f559e7d07b2efc00c5d32ac21bcae71f0
evenyoung <yiyun@igola.com>
2019-08-30 06:08:29 +0800

    1. Two Sum: add comment

--- src/datastructure/easy/TwoSum.java
+++ src/datastructure/easy/TwoSum.java
@@ -12,6 +12,8 @@ package datastructure.easy;
  * Given nums = [2, 7, 11, 15], target = 9,
  * Because nums[0] + nums[1] = 2 + 7 = 9,
  * return [0, 1].
+ *
+ * TODO: Consider the way of refactoring
  */
 public class TwoSum {
 
