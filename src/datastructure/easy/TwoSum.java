165bb04bec8b916883f4001cbc24ceec311f6307
evenyoung <yiyun@igola.com>
2019-08-30 06:03:19 +0800

    1. Two Sum

---
+++ src/datastructure/easy/TwoSum.java
@@ -0,0 +1,32 @@
+package datastructure.easy;
+
+/**
+ * Created by Yiyun On 2019/8/30 06:01
+ *
+ * 1. Two Sum
+ *
+ * Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+ * You may assume that each input would have exactly one solution, and you may not use the same element twice.
+ *
+ * Example:
+ * Given nums = [2, 7, 11, 15], target = 9,
+ * Because nums[0] + nums[1] = 2 + 7 = 9,
+ * return [0, 1].
+ */
+public class TwoSum {
+
+    public int[] twoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < nums.length; j++) {
+                if (i == j)
+                    continue;
+
+                int sum = nums[i] + nums[j];
+                if (sum == target) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        return new int[]{0, 0};
+    }
+}
