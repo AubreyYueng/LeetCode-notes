681f8dad951b837f8f775a390e4c0dd560aeb504
evenyoung <yiyun@igola.com>
2020-05-23 01:14:11 -0400

    23. Merge k Sorted Lists

---
+++ src/linkedlist/hard/MergeKSortedLists.java
@@ -0,0 +1,43 @@
+package linkedlist.hard;
+
+import linkedlist.ListNode;
+
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+/**
+ * Created by Yiyun On 2020/5/22 23:55
+ *
+ * 23. Merge k Sorted Lists
+ */
+public class MergeKSortedLists {
+
+    public ListNode mergeKLists(ListNode[] lists) {
+        // The head of this queue is the least element with respect to the specified ordering. If multiple elements
+        // are tied for least value, the head is one of those elements — ties are broken arbitrarily
+
+        // Here's the optimized one by one version using PQ
+        PriorityQueue<ListNode> pq = new PriorityQueue<>(Comparator.comparing(ln -> ln.val));
+        for (ListNode l : lists) {
+            if (l != null)
+                pq.add(l);
+        }
+
+        // Remember this way of initialization to simplify the complexity!!!
+        ListNode head = new ListNode(0);
+        ListNode curr = head;
+        while (!pq.isEmpty()) {
+            // actually we don't need extra space here, simply we set tmp = pq.poll() is also ok.
+            ListNode peek = pq.poll();
+            ListNode tmp = new ListNode(peek.val);
+            curr.next = tmp;
+            curr = curr.next;
+
+            if (peek.next != null)
+                pq.add(peek.next);
+        }
+
+        return head.next;
+    }
+
+}
