d3f9e9921d7c5208d24d3504a27ff675e752ff76
evenyoung <yiyun@igola.com>
2020-06-10 01:35:53 -0400

    32. Longest Valid Parentheses(DP: not recommended)

--- src/stack/hard/LongestValidParentheses.java
+++ src/stack/hard/LongestValidParentheses.java
@@ -13,9 +13,40 @@ import static org.junit.Assert.assertEquals;
  */
 public class LongestValidParentheses {
 
+    // Approach 2: DP (I think it's more complicated than Approach 2 using stack,
+    // solving in stack even without seeing the solution...)
+    // 1. state: dp[s_len], dp [i] represents the maxLen the valid subStr ends with s[i],
+    // thus all dp[i] is 0 if s[i] is '('.  NOTE, it's NOT the maxLen of first N characters
+    // 2. init: all 0
+    // 3. func:
+    // if s[i] = '(', dp[i] = 0
+    // else:
+    //    if s[i-1] = '(', dp[i] = dp[i-2]+2.
+    //    else if s[i-1-dp[i-1]] = '(', dp[i] = dp[i-2-dp[i-1]] + dp[i-1] + 2
+    // 4. return:  max_len compared during iteration
+    public int longestValidParentheses(String s) {
+        s = "))"+s;
+        int[] dp = new int[s.length()];
+        int res = 0;
+        for (int i = 2; i < s.length(); i++) {
+            if (s.charAt(i) == '(')
+                continue;
+
+            if (s.charAt(i-1)=='(') {
+                dp[i] = dp[i-2]+2;
+            } else if (s.charAt(i-1-dp[i-1]) == '(') {
+                dp[i] = dp[i - 1] + dp[i-2-dp[i-1]] + 2;
+            }
+
+            res = Math.max(res, dp[i]);
+        }
+
+        return res;
+    }
+
     // Approach 1: Stack
     // I think it's similar to monotonic stack from ) to (.
-    public int longestValidParentheses(String s) {
+    public int longestValidParentheses_stack(String s) {
         int res = 0;
         LinkedList<Integer> stack = new LinkedList<>();
         // because all items could be popped while processing is not finished,
