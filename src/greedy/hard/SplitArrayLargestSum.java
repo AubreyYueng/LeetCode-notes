91a37a4fa9ed6943eac2f1987a0a70f8df9a8224
evenyoung <yiyun@igola.com>
2020-06-22 01:36:10 -0400

    410. Split Array Largest Sum(Binary+Greedy)

--- src/greedy/hard/SplitArrayLargestSum.java
+++ src/greedy/hard/SplitArrayLargestSum.java
@@ -13,6 +13,55 @@ import static org.junit.Assert.assertEquals;
  */
 public class SplitArrayLargestSum {
 
+    // Solution 2: Binary Search + Greedy
+    public int splitArray(int[] nums, int m) {
+        long n = nums.length;
+        long l = Arrays.stream(nums).min().orElse(0);
+        long r = 0;
+        for (int num : nums) {
+            r += num;
+        }
+
+        // Answer stores possible maximum sub array sum
+        long answer = r;
+        while (l <= r) {
+            long mid = (l + r) / 2;
+            // If mid is possible solution, put answer = mid;
+            if (check(mid, nums, n, m)) {
+                answer = mid;
+                r = mid - 1;
+            } else {
+                l = mid + 1;
+            }
+        }
+        return (int)answer;
+    }
+
+    // Function to check if mid can be maximum sub-arrays sum
+    private boolean check(long mid, int nums[], long n, int m) {
+        long cnt = 1;
+        long sum = 0;
+        for (int i = 0; i < n; i++) {
+            // Because all numbers are positive, if an element is greater than maximum possible sum, then return false
+            if (nums[i] > mid)
+                return false;
+
+            long sumTmp = sum + nums[i];
+            // If the sum is greater than mid, increase count
+            if (sumTmp > mid) {
+                sum = nums[i];
+                cnt++;
+            } else {
+                // Increase sum of current sub-array
+                sum = sumTmp;
+            }
+
+            if (cnt > m)
+                return false;
+        }
+        return true;
+    }
+
     // Solution 1: DP, reason: non-after effect, i.e., once the state of a certain stage is determined,
     // it is not affected by the state in the future
     // state: [i][j], minimum largest array sum for splitting nums[0,...,i] into j parts
@@ -23,7 +72,7 @@ public class SplitArrayLargestSum {
     // init: [n+1][m+1], default to maxInt except [0][0]
     //
     // Trick: let sum[i] for sum over first i numbers, then nums[k+1]+...+nums[i] = sum[i] - sum[k]
-    public int splitArray(int[] nums, int m) {
+    public int splitArray_dp(int[] nums, int m) {
         int n = nums.length;
 
         int[][] dp = new int[n+1][m+1];
