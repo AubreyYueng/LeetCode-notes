a1d5cfd4bc6cb8f5bec46c6e42ef0b4abe67ba3a
evenyoung <yiyun@igola.com>
2020-07-06 01:21:28 -0400

    Review: 31. Next Permutation

--- src/array/medium/NextPermutation.java
+++ src/array/medium/NextPermutation.java
@@ -1,5 +1,11 @@
 package array.medium;
 
+import org.junit.Test;
+
+import java.util.Arrays;
+
+import static org.junit.Assert.assertEquals;
+
 /**
  * Created by Yiyun On 2020/6/25 03:55
  *
@@ -7,6 +13,39 @@ package array.medium;
  * lexicographically: in dictionary order
  */
 public class NextPermutation {
+
+    public void nextPermutation_review20200706(int[] nums) {
+        if (nums == null || nums.length <= 1)
+            return;
+        int n = nums.length;
+
+        // 1. find the start of descending ordered sequence
+        // Note: 1) should query reversely
+        //       2) i-- even when nums[i]==nums[i+1](eg. input[5,1,1])
+        int i = n-2;
+        while (i >= 0 && nums[i] >= nums[i+1]) i--;
+
+        // 2. swap nums[i] with the next larger number(querying reversely)
+        // Note: don't forget this 'if' condition
+        if (i >= 0) swapWithNextLarger(nums, i);
+
+        // 3. reverse numbers from the peak
+        int l = i+1;
+        int r = n-1;
+        while (l < r) {
+            swap(nums, l++, r--);
+        }
+    }
+
+    private void swapWithNextLarger(int[] nums, int i) {
+        int j = nums.length;
+        while (--j > i) {
+            if (nums[j] > nums[i]) {
+                swap(nums, i, j);
+                break;
+            }
+        }
+    }
 
     // permutations are in ascending order
     // eg. given 1324, then next should be 1342
@@ -46,6 +85,55 @@ public class NextPermutation {
             st++;
             ed--;
         }
+    }
+
+    @Test
+    public void case1() {
+        int nums[] = new int[]{2, 1, 3};
+        nextPermutation_review20200706(nums);
+        assertEquals("[2, 3, 1]", Arrays.toString(nums));
+    }
+
+    @Test
+    public void case2() {
+        int nums[] = new int[]{1, 2, 3};
+        nextPermutation_review20200706(nums);
+        assertEquals("[1, 3, 2]", Arrays.toString(nums));
+    }
+
+    @Test
+    public void case3() {
+        int nums[] = new int[]{1, 1, 5};
+        nextPermutation_review20200706(nums);
+        assertEquals("[1, 5, 1]", Arrays.toString(nums));
+    }
+
+    @Test
+    public void case4() {
+        int nums[] = new int[]{2, 3, 1};
+        nextPermutation_review20200706(nums);
+        assertEquals("[3, 1, 2]", Arrays.toString(nums));
+    }
+
+    @Test
+    public void case5() {
+        int nums[] = new int[]{1, 3, 2};
+        nextPermutation_review20200706(nums);
+        assertEquals("[2, 1, 3]", Arrays.toString(nums));
+    }
+
+    @Test
+    public void case6() {
+        int nums[] = new int[]{3, 2, 1};
+        nextPermutation_review20200706(nums);
+        assertEquals("[1, 2, 3]", Arrays.toString(nums));
+    }
+
+    @Test
+    public void case7() {
+        int nums[] = new int[]{5, 1, 1};
+        nextPermutation_review20200706(nums);
+        assertEquals("[1, 1, 5]", Arrays.toString(nums));
     }
 
 }
