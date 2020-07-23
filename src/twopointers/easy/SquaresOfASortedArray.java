ef5f8508fb85c9aa8754ed6df432ddd143221fd9
evenyoung <yiyun@igola.com>
2020-07-22 23:25:30 -0400

    977. Squares of a Sorted Array

---
+++ src/twopointers/easy/SquaresOfASortedArray.java
@@ -0,0 +1,42 @@
+package twopointers.easy;
+
+/**
+ * Created by Yiyun On 2020/7/22 23:22
+ *
+ * 977. Squares of a Sorted Array
+ */
+public class SquaresOfASortedArray {
+
+    public int[] sortedSquares(int[] A) {
+        int N = A.length;
+        int j = 0;
+        while (j < N && A[j] < 0)
+            j++;
+        int i = j-1;
+
+        int[] ans = new int[N];
+        int t = 0;
+
+        while (i >= 0 && j < N) {
+            if (A[i] * A[i] < A[j] * A[j]) {
+                ans[t++] = A[i] * A[i];
+                i--;
+            } else {
+                ans[t++] = A[j] * A[j];
+                j++;
+            }
+        }
+
+        while (i >= 0) {
+            ans[t++] = A[i] * A[i];
+            i--;
+        }
+        while (j < N) {
+            ans[t++] = A[j] * A[j];
+            j++;
+        }
+
+        return ans;
+    }
+
+}
