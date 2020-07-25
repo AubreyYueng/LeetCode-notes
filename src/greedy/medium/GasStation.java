b24e05bd1a9e1bd617107c4496899b639f23ac8a
evenyoung <yiyun@igola.com>
2020-07-24 23:01:28 -0400

    Review: 134. Gas Station

--- src/greedy/medium/GasStation.java
+++ src/greedy/medium/GasStation.java
@@ -7,6 +7,22 @@ package greedy.medium;
  */
 public class GasStation {
 
+    public int canCompleteCircuit_review20200724(int[] gas, int[] cost) {
+        int tmp = 0;
+        int st = 0;
+        int total = 0;
+        for (int i = 0; i < cost.length; i++) {
+            int curr = gas[i] - cost[i];
+            tmp += curr;
+            total += curr;
+            if (tmp < 0) {
+                st = i+1;
+                tmp = 0;
+            }
+        }
+        return total < 0 ? -1 : st;
+    }
+
     // If at some station curr_tank is less than 0, mark this station as a new starting point,
     // reset curr_tank to zero. Check total gas at last.
     // Reason: It's not hard to think that, when starts from i, one can go furthest to j, then one can't go further
