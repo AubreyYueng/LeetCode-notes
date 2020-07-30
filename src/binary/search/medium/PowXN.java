c5f4a94956330cb94f51ba25242f9a70c4694213
evenyoung <yiyun@igola.com>
2020-07-29 22:48:06 -0400

    Review: 50. Pow(x, n)

--- src/binary/search/medium/PowXN.java
+++ src/binary/search/medium/PowXN.java
@@ -1,11 +1,35 @@
 package binary.search.medium;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 /**
  * Created by Yiyun On 2020/7/5 16:59
  *
  * 50. Pow(x, n)
  */
 public class PowXN {
+
+    public double myPow_review20200729(double x, int n) {
+        // WARN: simply negate a positive int n might lead to overflow error, thus we need to define a long for n.
+        // i.e. DO NOT write as: if (n < 0) return myPow(x, -n).
+        long N = n;
+        if (N < 0) {
+            x = 1/x;
+            N = -N;
+        }
+
+        double currX = x;
+        double res = 1;
+        while (N > 0) {
+            long lsb = N & 1;    // least significant bit
+            N >>= 1;
+            if (lsb == 1) res *= currX;
+            currX *= currX;
+        }
+        return res;
+    }
 
     // n could be negative
     // eg.x^10 = x^{(1010)_2}
@@ -30,6 +54,11 @@ public class PowXN {
         }
 
         return res;
+    }
+
+    @Test
+    public void case1() {
+        assertEquals(1024, myPow_review20200729(2.0, 10), 0.00001);
     }
 
 }
