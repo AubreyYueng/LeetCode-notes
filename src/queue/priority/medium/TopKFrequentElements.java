01bcd45d9cd986d64b44819a447fbb982b913f57
evenyoung <yiyun@igola.com>
2020-07-29 23:49:45 -0400

    Review: 347. Top K Frequent Elements(Quick Select)

--- src/queue/priority/medium/TopKFrequentElements.java
+++ src/queue/priority/medium/TopKFrequentElements.java
@@ -13,6 +13,83 @@ import java.util.Random;
  */
 public class TopKFrequentElements {
 
+    public int[] topKFrequent_review20200729_quickselect(int[] nums, int k) {
+        return new QuickSelect_20200729(nums, k).res;
+    }
+
+    private static class QuickSelect_20200729 {
+        private static class Item {
+            int num;
+            int freq;
+
+            public Item(int num, int freq) {
+                this.num = num;
+                this.freq = freq;
+            }
+        }
+
+        Item[] items;
+        int[] res;
+        int k;
+
+        QuickSelect_20200729(int[] nums, int k) {
+            this.k = k;
+
+            HashMap<Integer, Integer> frequencies = toFreqMap(nums);
+            int n = frequencies.size();
+            items = new Item[n];
+            int i = 0;
+            for (Integer key : toFreqMap(nums).keySet()) {
+                items[i++] = new Item(key, frequencies.get(key));
+            }
+
+            quickSelect(0, n-1);
+            res = new int[k];
+            for (int j = 0; j < k; j++) {
+                res[j] = items[j].num;
+            }
+        }
+
+        private void quickSelect(int st, int ed) {
+            if (st >= ed) return;
+            int selected = new Random().nextInt(ed-st) + st;
+            int pivot = partition(selected, st, ed);
+            if (pivot == k) return;
+            if (pivot < k)
+                quickSelect(pivot+1, ed);
+            else
+                quickSelect(st, pivot-1);
+        }
+
+        private int partition(int selected, int st, int ed) {
+            int selectedFreq = items[selected].freq;
+            swap(selected, ed);
+
+            int stored = st;
+            for (int i = st; i < ed; i++) {
+                if (items[i].freq > selectedFreq)
+                    swap(stored++, i);
+            }
+            swap(stored, ed);
+            return stored;
+        }
+
+        private void swap(int i, int j) {
+            Item tmp = items[j];
+            items[j] = items[i];
+            items[i] = tmp;
+        }
+
+        private static HashMap<Integer, Integer> toFreqMap(int[] nums) {
+            HashMap<Integer, Integer> frequencies = new HashMap<>();
+            for (int num : nums) {
+                int cnt = frequencies.getOrDefault(num, 0)+1;
+                frequencies.put(num, cnt);
+            }
+            return frequencies;
+        }
+    }
+
     public int[] topKFrequent(int[] nums, int k) {
         HashMap<Integer, Integer> cntMap = new HashMap<>();
         for (int n : nums) {
@@ -81,6 +158,6 @@ public class TopKFrequentElements {
 
     @Test
     public void case1() {
-        System.out.println(Arrays.toString(topKFrequent(new int[]{1,1,1,2,2,3}, 2)));
+        System.out.println(Arrays.toString(topKFrequent_review20200729_quickselect(new int[]{1,1,1,2,2,3}, 2)));
     }
 }
