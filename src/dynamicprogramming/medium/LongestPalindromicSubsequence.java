eb092540b1285f56b37b560352322eadd263e155
Yiyun Yang <evenyoung2012@gmail.com>
2020-09-12 18:31:04 -0400

    review: 516. Longest Palindromic Subsequence

--- src/dynamicprogramming/medium/LongestPalindromicSubsequence.java
+++ src/dynamicprogramming/medium/LongestPalindromicSubsequence.java
@@ -1,5 +1,7 @@
 package dynamicprogramming.medium;
 
+import java.util.Arrays;
+
 /**
  * Created by Yiyun On 2020/2/6 17:02
  *
@@ -8,10 +10,44 @@ package dynamicprogramming.medium;
  */
 public class LongestPalindromicSubsequence {
 
+    // this dp is looped by distance, thus we can do a bottom-up dp.
+    // and the state dp[i, j] does not refer to the palindrome length starts at [i] and ends at [j],
+    // BUT the longest palindrome length between [i] and [j].
+    // For bottom-up, we can do the analysis by distance=0,1,2... and each of which has 2 cases: eq/neq.
+    public int longestPalindrome_review20200912(String s) {
+        if (s == null) return 0;
+        int n = s.length();
+        if (n <= 1) return n;
+
+        // dp[i] for len of longest palindrome between [i] and [i+d]
+        // d=0,1,2... thus dp[] is updated accordingly.
+        int[] prevDp = new int[n];
+        int[] dp = new int[n];
+        Arrays.fill(dp, 1);
+
+        for (int dist = 1; dist < n; dist++) {
+            System.out.println("dist: " + (dist-1) + ", dp: " + Arrays.toString(dp));
+
+            int[] dpOfCurrDist = new int[n];
+            for (int i = 0; i < n - dist; i++) {
+                char chI = s.charAt(i);
+                char chJ = s.charAt(i + dist);
+                if (chI == chJ)
+                    dpOfCurrDist[i] = prevDp[i+1] + 2;
+                else
+                    dpOfCurrDist[i] = Math.max(dp[i], dp[i+1]);
+            }
+            prevDp = dp;
+            dp = dpOfCurrDist;
+        }
+
+        return Math.max(dp[0], dp[1]);
+    }
+
     public int longestPalindromeSubseq(String s) {
         // f(i, j): longest len between palindrome subseq from index i to j
         // f(i, j) = f(i+1, j-1) + 2;           if (ch_i == ch_j)
-        //         = Max(f(i+1, k), f(i, j-1)); if (ch_i != ch_j)
+        //         = Max(f(i+1, j), f(i, j-1)); if (ch_i != ch_j)
         //         = 1;                         if (i == j)
         int len = s.length();
         int[][]dp = new int[len][len];
