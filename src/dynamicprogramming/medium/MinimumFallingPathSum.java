22e9ceb959fc4b4730d4704b8ac0f3bfbfa04b14
Yiyun Yang <evenyoung2012@gmail.com>
2020-09-12 21:21:17 -0400

    review: 931. Minimum Falling Path Sum

--- src/dynamicprogramming/medium/MinimumFallingPathSum.java
+++ src/dynamicprogramming/medium/MinimumFallingPathSum.java
@@ -1,5 +1,7 @@
 package dynamicprogramming.medium;
 
+import java.util.Arrays;
+
 /**
  * Created by Yiyun On 2020/1/28 11:08
  *
@@ -7,6 +9,26 @@ package dynamicprogramming.medium;
  * https://leetcode.com/problems/minimum-falling-path-sum/
  */
 public class MinimumFallingPathSum {
+
+    // dp[i, j]: min falling path sum of each cell limited in the first i rows
+    public int minFallingPathSum_review20200912(int[][] A) {
+        if (A == null || A.length == 0) return 0;
+        int m = A.length;
+        int n = A[0].length;
+
+        int[] dp = Arrays.copyOf(A[0], n);
+        for (int i = 1; i < m; i++) {
+            int[] currDp = Arrays.copyOf(A[i], n);
+            for (int j = 0; j < n; j++) {
+                int tmp = dp[j];
+                if (j > 0) tmp = Math.min(tmp, dp[j-1]);
+                if (j < n-1) tmp = Math.min(tmp, dp[j+1]);
+                currDp[j] += tmp;
+            }
+            dp = currDp;
+        }
+        return Arrays.stream(dp).min().getAsInt();
+    }
 
     public int minFallingPathSum(int[][] A) {
         int row = A.length;
